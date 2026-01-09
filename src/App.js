61d8aa83dd2b272a0d6b5ed3a4004b8ee9adc47c
Jeongsik <idjs1102@naver.com>
2026-01-09 16:59:40 +0900

    Page Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import RouteTest from './components/RouteTest.js';
+import Home from './pages/Home.js';
+import New from './pages/New.js';
+import Edit from './pages/Edit.js';
+import Diary from './pages/Diary.js';
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/new" element={<New />}/>
+          <Route path="/edit" element={<Edit />}/>
+          <Route path="/diary/:id" element={<Diary />}/>
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Edit.js
@@ -0,0 +1,24 @@
+import {useSearchParams, useNavigate} from "react-router-dom";
+
+const Edit = () => {
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const id = searchParams.get('id');
+    const mode = searchParams.get('mode');
+
+    console.log(id);
+    console.log(mode);
+
+    return (
+        <div>
+            <h1>Edit</h1>
+            <p>이곳은 일기 수정 페이지입니다.</p>
+            <button onClick={() => {setSearchParams({who: 'jeongsik'})}}>QS 바꾸기</button>
+            <button onClick={() => {navigate("/home");}}>HOME으로 가기</button>
+            <button onClick={() => {navigate(-1);}}>뒤로 가기</button>
+        </div>
+    );
+};
+
+export default Edit;
